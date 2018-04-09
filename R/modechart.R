2a952c4737d76fc5667c9877b4e1fafffe3c0eb0
akrinaldi <35223187+akrinaldi@users.noreply.github.com>
2018-04-09 19:10:47 -0400

    Update modechart.R

--- R/modechart.R
+++ R/modechart.R
@@ -1,4 +1,15 @@
+#' modechart
+#'
+#' This is a function that allows a user make a table of the mode values of
+#' three different variables within a dataset.
+#'
+#' @param x The first variable of a dataset the user wants to record mode for
+#' @param y The next variable of a dataset the user wants to record mode for
+#' @param z The third variable of a dataset the user wants to record mode for
+#' @return A chart of the mode values for each parameter
 #' @export
-modechart<-function(x,y,z){
- data.frame(mymode(x),mymode(y),mymode(z))
+#' @examples
+#' modechart(mtcars$mpg,mtcars$wt,mtcars$cyl)
+modechart <- function(x,y,z){
+  data.frame(mymode(x),mymode(y),mymode(z))
 }
