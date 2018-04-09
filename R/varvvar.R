7bf6d7d67d975c61bacec8e435bb2e7a77b2c075
akrinaldi <35223187+akrinaldi@users.noreply.github.com>
2018-04-09 19:11:48 -0400

    Update varvvar.R

--- R/varvvar.R
+++ R/varvvar.R
@@ -1,4 +1,14 @@
+#' varvvar
+#'
+#' This is a function that creates a graph of values from two variables
+#' in a dataset versus each other using ggplot2.
+#'
+#' @param x The first variable of a dataset that's values are being plotted
+#' @param y The second variable of a dataset that's values are being plotted
+#' @return A graph of points
 #' @export
+#' @examples
+#' varvvar(mtcars$mpg,mtcars$wt)
 varvvar<-function(x,y){
- qplot(x,y,geom = "point")
+  qplot(x,y,geom = "point")
 }
