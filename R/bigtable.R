80778b4a95048b81ba27c7c938aae7fe4f99469b
akrinaldi <35223187+akrinaldi@users.noreply.github.com>
2018-04-09 19:08:42 -0400

    Update bigtable.R

--- R/bigtable.R
+++ R/bigtable.R
@@ -1,4 +1,15 @@
+#' bigtable
+#'
+#' This is a function that allows a user make a table of the frequencies of
+#' two different variables within a dataset.
+#'
+#' @param x The first variable of a dataset the user wants to record frequencies for
+#' @param y The second variable of a dataset the user wants to record frequencies for
+#' @return A chart of the frequency values for each parameter together
 #' @export
-bigtable<-function(x,y){
-c(table(x),table(y))
+#' @examples
+#' bigtable(mtcars$mpg,mtcars$wt)
+#' Must have same number of rows
+bigtable <- function(x,y){
+  c(table(x),table(y))
 }
