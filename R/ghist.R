0b3dbf615e53dc38eb0ba1955f82eb72e78877e9
akrinaldi <35223187+akrinaldi@users.noreply.github.com>
2018-04-09 19:10:21 -0400

    Update ghist.R

--- R/ghist.R
+++ R/ghist.R
@@ -1,4 +1,13 @@
+#' ghist
+#'
+#' This is a function that creates a histogram of frequency values from a variable
+#' in a dataset using ggplot2.
+#'
+#' @param x The variable of a dataset that's frequency values are being plotted
+#' @return A histogram
 #' @export
+#' @examples
+#' ghist(mtcars$mpg)
 ghist<-function(x){
- qplot(x,geom = "histogram",binwidth=0.5)
+  qplot(x,geom = "histogram",binwidth=0.5)
 }
