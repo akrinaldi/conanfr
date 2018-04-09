b22da452253194b640813454388dbc9cd19d551c
akrinaldi <35223187+akrinaldi@users.noreply.github.com>
2018-04-09 19:11:10 -0400

    Update mymode.R

--- R/mymode.R
+++ R/mymode.R
@@ -1,5 +1,16 @@
+#' mymode
+#'
+#' This is a function that allows a user to find the mode of a variable
+#' in a dataset.
+#'
+#' Code taken from https://www.tutorialspoint.com/r/r_mean_median_mode.htm
+#'
+#' @param x The variable of a dataset the user wants to find the mode for
+#' @return A value (mode)
 #' @export
-mymode<-function(x){
+#' @examples
+#' mymode(mtcars$mpg)
+mymode <- function(x){
   uniqx<-unique(x)
   uniqx[which.max(tabulate(match(x,uniqx)))]
 }
