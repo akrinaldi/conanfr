a46f1f5235449fd9bc35a2af326f08851c6a3269
akrinaldi <35223187+akrinaldi@users.noreply.github.com>
2018-04-09 19:11:31 -0400

    Update myrange.R

--- R/myrange.R
+++ R/myrange.R
@@ -1,4 +1,13 @@
+#' myrange
+#'
+#' This is a function that allows a user to find the range of a variable
+#' in a dataset.
+#'
+#' @param x The variable of a dataset the user wants to find the range for
+#' @return A difference value (range)
 #' @export
-myrange<-function(x){
- max(x)-min(x)
+#' @examples
+#' myrange(mtcars$mpg)
+myrange <- function(x){
+  max(x)-min(x)
 }
